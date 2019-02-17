b51bd65053d2daed3d1ec47d40ab282406e8f81b
12miechy <12miechy@gmail.com>
2019-02-17 19:07:53 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
+import { RaisedButton } from "material-ui";
+import TextField from "material-ui/TextField";
 
 import { postEvent } from "../actions";
 
@@ -20,10 +22,14 @@ class EventsNew extends Component {
     } = field;
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     );
   }
 
@@ -34,7 +40,7 @@ class EventsNew extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
-
+    const style = { margin: 12 };
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -44,8 +50,6 @@ class EventsNew extends Component {
             type="text"
             component={this.renderField}
           />
-        </div>
-        <div>
           <Field
             label="Body"
             name="body"
@@ -53,14 +57,17 @@ class EventsNew extends Component {
             component={this.renderField}
           />
         </div>
-        <div>
-          <input
-            type="submit"
-            value="Submit"
-            disabled={pristine || submitting || invalid}
-          />
-          <Link to="/">Cancel</Link>
-        </div>
+        <RaisedButton
+          label="Submit"
+          type="submit"
+          style={style}
+          disabled={pristine || submitting || invalid}
+        />
+        <RaisedButton
+          label="Cancel"
+          style={style}
+          containerElement={<Link to="/" />}
+        />
       </form>
     );
   }
